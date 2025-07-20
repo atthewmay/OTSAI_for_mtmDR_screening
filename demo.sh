61dee862ccd01ab0b56621ecf2a86eceeed5874c
Matthew Hunt <huntm@wustl.edu>
2025-07-20 16:52:33 -0400

    .

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 # source ~/.zshrc  # Source .zshrc to load the run_python function
 #
 #
-./03_run_trials.sh ./run_trials_11_10_24.sh "gpt-4o-2024-08-06,gpt-4o-mini" &&
+./03_run_trials.sh "gpt-4o-2024-08-06,gpt-4o-mini" &&
 python code_files/04_grade_output_dir.py --output_dir outputs/model_run_demo_7_17_25 &&
 #
 # Can run the following, but nicer in Rstudio for display
